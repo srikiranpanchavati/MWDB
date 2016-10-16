0f55c32b9ee0bb36cae73cb7e2834646430ce87d
srikiranpanchavati <srikiran.phoenix@gmail.com>
2016-10-15 21:02:42 -0700

    added pca for task3 and updated helper

---
+++ Phase2/src/utils/helper.py
@@ -0,0 +1,10 @@
+import numpy as np
+class Helper:
+    def __init__(self):
+        pass
+
+    def get_histogram(self, path):
+        file = open('newfile.txt', 'r')
+        for line in file:
+            print line
+        pass
