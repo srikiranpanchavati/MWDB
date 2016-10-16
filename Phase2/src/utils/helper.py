89fad04e2aab2097d6b1c028404fbc0f8f6b1b0b
srikiranpanchavati <srikiran.phoenix@gmail.com>
2016-10-16 15:41:55 -0700

    updated PCA to get eigen values and vectors

--- Phase2/src/utils/helper.py
+++ Phase2/src/utils/helper.py
@@ -1,10 +1,21 @@
 import numpy as np
+
+
 class Helper:
     def __init__(self):
         pass
 
     def get_histogram(self, path):
-        file = open('newfile.txt', 'r')
+        file = open(path, 'r')
+        histogram_list = []
         for line in file:
-            print line
-        pass
+            line_list = []
+            hist_data = line.split(";")
+            line_list.append(hist_data[0])
+            line_list.append(hist_data[1])
+            line_list.append(hist_data[2])
+            hist_data[3] = hist_data[3].replace("\\n", "").replace(" ", "").replace("[", "").replace("]","")
+            line_list.append(map(int, hist_data[3].split(",")))
+            histogram_list.append(line_list)
+
+        return histogram_list
