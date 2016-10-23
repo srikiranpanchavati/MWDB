1901ebb842c8e51c76b4dbaf622023d0f0b1b418
srikiranpanchavati <srikiran.phoenix@gmail.com>
2016-10-23 10:34:42 -0700

    updated output dir path for search results

--- Phase2/src/task2/SubsequenceSearch.py
+++ Phase2/src/task2/SubsequenceSearch.py
@@ -13,6 +13,9 @@ class SubsequenceSearch:
         self.b = end
         self.k = k
         self.fetures_path = feature_path
+        self.out_path = os.path.join(self.directory_path, self.video_name.split(".")[0])
+        if not os.path.exists(self.out_path):
+            os.makedirs(self.out_path)
 
     def save_video_sequence(self, video_name=None, start=-1, end=-1, rank=-1):
         if video_name is None:
@@ -21,16 +24,20 @@ class SubsequenceSearch:
             start = self.a
         if end == -1:
             end = self.b
+
         out_file_name = video_name.split(".")[0]
+
+
         if rank == -1:
             out_file_name += "_search_sequence"
         else:
-            out_file_name += "_result_" + str(rank)
+            out_file_name = str(rank) + "_" + out_file_name  + "_result_"
 
         video_path = self.directory_path + "\\" + video_name
+
         video = cv2.VideoCapture(video_path)
         fourcc = cv2.cv.CV_FOURCC(*'XVID')
-        out = cv2.VideoWriter(self.directory_path + "\\out\\" + out_file_name + ".avi", fourcc, video.get(cv2.cv.CV_CAP_PROP_FPS),
+        out = cv2.VideoWriter(self.out_path + "\\" + out_file_name + ".avi", fourcc, video.get(cv2.cv.CV_CAP_PROP_FPS),
                               (int(video.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)),
                                int(video.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))))
         frame_no = 0
@@ -96,13 +103,14 @@ class SubsequenceSearch:
 
 
 if __name__ == "__main__":
-    dir_path = "C:\\Users\\Giridhar\\Desktop\\MWDB Project\\P2DemoVideos"#raw_input("Enter input directory path: ")
+    dir_path = "G:\\CSE515\\P2DemoVideos"#raw_input("Enter input directory path: ")
     vname = "6x_SQ_TL_BR_Check.mp4" #raw_input("Enter video file name: ")
-    method_name = "MOTION_SIM1"#raw_input("Enter method name to be used for search: ")
+    method_name = "MOTION_SIM2"#raw_input("Enter method name to be used for search: ")
     start_index = 1#raw_input("Enter start frame number: ")
     end_index = 15#raw_input("Enter end frame number: ")
     no_of_sequences = 5#raw_input("Enter number of sequences (K): ")
-    feature_file  = "C:\\Users\\Giridhar\\Desktop\\MWDB Project\\MWDB\\Phase2\\sample_data\\input\\motionVector_2r.mvect"#raw_input("Enter feature file path: ")
+    feature_file  = "G:\\CSE515\\GitRepo\\MWDB\\Phase2\\sample_data\\input\\motionVector_2r.mvect"#raw_input("Enter feature file path: ")
 
     ssearch = SubsequenceSearch(dir_path, vname, feature_file, method_name, int(start_index), int(end_index), int(no_of_sequences))
+    ssearch.save_video_sequence(None, start_index, end_index)
     ssearch.find_similar_frames()
