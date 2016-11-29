d8f73a56b703d6293cb8c43c2dbbf93f3c6d8ce8
Giridhar Peddabuttaiahgari <2giridhar.reddy@gmail.com>
2016-11-28 21:04:57 -0700

    task2 update

--- Phase3/src/task2/SimilarityGraph.py
+++ Phase3/src/task2/SimilarityGraph.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.spatial import distance
 import sys
 import operator
+import ntpath
 
 
 def vector_distance(vector1, vector2):
@@ -58,15 +59,65 @@ def knearest_frames(k, sim_arr):
                 break
     return out_sim
 
-def find_k_nearest_frames(inp_arr):
+def kframes_similarity(feature_arr, k, out_file_name):
     """
     Find K most similar frames for each frame in the file
     :param inp_arr:
     :return: 2d array {(vs,fs),(vd,fd),sim(a,b)}
     """
+    sim_matrix = sim_of_frames(feature_arr[:,-1])
+    knearest_frames(k, sim_matrix)
 
-    pass
 
+def read_file(inp_path):
+    video_features = {}
+    for line in open(inp_path):
+        feature_list = line.split(",")
+        key  = (feature_list[0], feature_list[1])
+        if key not in video_features:
+            video_features[key] = []
+        features = video_features[key]
+        feature = []
+        feature.append(float(feature_list[2]))
+        feature.append(float(feature_list[3]))
+        feature.append(float(feature_list[4]))
+        sift_info = feature_list[5].translate(None, '[]')
+        desc = sift_info.split()
+        for f in desc:
+            feature.append(float(f))
+        features.append(feature)
+    vf_list = []
+    for key,value in video_features:
+        vf_list.append([key,value])
+    print(vf_list)
+    return vf_list
+
+# def read_file(inp_path):
+#     video_features = {}
+#     for line in open(inp_path):
+#         feature_list = line.split(",")
+#         video_key = feature_list[0]
+#         frame_key = feature_list[1].strip()
+#         if video_key not in video_features:
+#             video_features[video_key] = {}
+#         frame_features = video_features[video_key]
+#         if frame_key not in frame_features:
+#             frame_features[frame_key] = []
+#         features = frame_features[frame_key]
+#         feature = []
+#         feature.append(float(feature_list[2]))
+#         feature.append(float(feature_list[3]))
+#         feature.append(float(feature_list[4]))
+#         sift_info = feature_list[5].translate(None, '[]')
+#         desc = sift_info.split()
+#         for f in desc:
+#             feature.append(float(f))
+#         features.append(feature)
+#     print(video_features)
+
+def get_file_name(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
 
 if __name__ == "__main__":
     """
@@ -75,4 +126,9 @@ if __name__ == "__main__":
     Store to file
 
     """
-    pass
+    inp_file_path = raw_input("Enter Sift file path:")
+    k = int(raw_input("Enter K value:"))
+    out_file = get_file_name(inp_file_path).split(".")[0] +"_"+str(k)+".gspc"
+    print(out_file)
+    video_features = read_file(inp_file_path)
+    kframes_similarity(video_features, k, out_file)
