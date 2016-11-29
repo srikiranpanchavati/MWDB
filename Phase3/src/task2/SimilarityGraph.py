95b924c9002497cf30fc606ae0564b098a810e43
Giridhar Peddabuttaiahgari <2giridhar.reddy@gmail.com>
2016-11-29 03:42:32 -0700

    task2 final commit

--- Phase3/src/task2/SimilarityGraph.py
+++ Phase3/src/task2/SimilarityGraph.py
@@ -10,12 +10,6 @@ def vector_distance(vector1, vector2):
 
 
 def sim_of_two_frames(frame1, frame2):
-    """
-    find similarity between two frames
-    :param frame1: 2d array of features
-    :param frame2: 2d array of features
-    :return: float in range 0 and 1
-    """
     total_dist = 0
     for x in frame1:
         min_distance = sys.maxint
@@ -30,25 +24,8 @@ def sim_of_two_frames(frame1, frame2):
     return sim
 
 
-# def sim_of_frames(frame_arr):
-#     """
-#
-#     :param frame_arr:
-#     :rtype: object
-#     """
-#     length = frame_arr.shape[1]
-#     sim_matrix = np.array(length, length)
-#     sim_matrix.fill(-1)
-#     for i in range(0, length):
-#         for j in range(0, length):
-#             if i != j:
-#                 sim = sim_of_two_frames(frame_arr[i], frame_arr[j])
-#                 sim_matrix[i, j] = sim
-    return sim_matrix
-
-
 def similar_frames(index, frame_arr, k):
-    length = frame_arr.shape[1]
+    length = frame_arr.shape[0]
     sim_list = []
     frame = frame_arr[index]
     for j in range(0, length):
@@ -61,51 +38,32 @@ def similar_frames(index, frame_arr, k):
     return k_nearest_frames(sim_list, k)
 
 
-def k_nearest_frames(K, sim_arr):
+def k_nearest_frames(sim_arr, K):
     sim_dict = dict(enumerate(sim_arr))
-    sorted_sim = sorted(sim_dict.items(), key=operator.itemgetter(1))
-    out_sim = np.array((K, 2))
+    sorted_sim = sorted(sim_dict.items(), key=operator.itemgetter(1), reverse=True)
+    out_sim = np.zeros((K, 2))
     i = 0
     for x in sorted_sim:
-        if x[1] > 0:
-            out_sim[i, 0] = x[0]
-            out_sim[i, 1] = x[1]
-            i += 1
-            if i == K:
-                break
+        out_sim[i, 0] = x[0]
+        out_sim[i, 1] = x[1]
+        i += 1
+        if i == K:
+            break
     return out_sim
 
 
 def k_similar_frames(feature_arr, k, out_file_name):
-    length = feature_arr.shape[1]
-    frame_arr = feature_arr[:, -1]
+    length = feature_arr.shape[0]
+    frame_arr = np.asarray(feature_arr[:, 1])
     file_writer = open(out_file_name, 'w')
     for i in range(0, length):
         sim_arr = similar_frames(i, frame_arr, k)
         for j in range(0, k):
-            str = feature_arr[i, 0] + ", "
-            str += feature_arr[j, 0] + ", "
-            str += sim_arr[i, j]
-            file_writer.writelines(str)
-
-
-# def k_frames_similarity(feature_arr, k, out_file_name):
-#     """
-#     Find K most similar frames for each frame in the file
-#     :param feature_arr:
-#     :rtype: object
-#     :param inp_arr:
-#     :return: 2d array {(vs,fs),(vd,fd),sim(a,b)}
-#     """
-#     sim_matrix = sim_of_frames(feature_arr[:, -1])
-#     file_writer = open(out_file_name, 'w')
-#     sim = k_nearest_frames(k, sim_matrix)
-#     for i in range(0, len(sim)):
-#         for j in range(0, len(sim[0])):
-#             str = feature_arr[i, 0] + ", "
-#             str += feature_arr[j, 0] + ", "
-#             str += sim[i, j]
-#             file_writer.writelines(str)
+            line = str(feature_arr[i, 0]) + ", "
+            line += str(feature_arr[sim_arr[j, 0], 0]) + ", "
+            line += str(sim_arr[j, 1])+'\n'
+            file_writer.write(line)
+    file_writer.close()
 
 
 def read_file(inp_path):
@@ -127,7 +85,7 @@ def read_file(inp_path):
         features.append(feature)
     vf_list = []
     for key, value in video_features:
-        vf_list.append([key, value])
+        vf_list.append([(key, value), video_features[(key, value)]])
     return np.asarray(vf_list)
 
 
@@ -137,11 +95,6 @@ def get_file_name(path):
 
 
 if __name__ == "__main__":
-    """
-    Read Input from the user <Dimension Reduced Sift File, K(Integer)>
-    Call method find_k_nearest_frames()
-    Store to file
-    """
     inp_file_path = raw_input("Enter Sift file path:")
     k = int(raw_input("Enter K value:"))
     out_file = get_file_name(inp_file_path).split(".")[0] + "_" + str(k) + ".gspc"
