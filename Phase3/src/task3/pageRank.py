32795fb5fb46acaacd51bb6e2a3dec8be730445b
Sumeet <sj.jainsumeet@gmail.com>
2016-11-30 18:22:34 -0700

    updated pagerank. bug fixed

--- Phase3/src/task3/pageRank.py
+++ Phase3/src/task3/pageRank.py
@@ -6,69 +6,33 @@ import os
 import cv2
 
 
-def google_matrix(G, alpha=0.85, personalization=None,
-                  nodelist=None, weight='weight', dangling=None):
+def calc_pagerank(D, weight='weight'):
+
+    alpha=0.85
+    tol=1.0e-6
 
 
+    W = nx.stochastic_graph(D, weight=weight)
+    N = W.number_of_nodes()
 
-    if nodelist is None:
-        nodelist = list(G)
+    x = dict.fromkeys(W, 1.0 / N)
+    dangling_weights = x
+    dangling_nodes = [n for n in W if W.out_degree(n, weight=weight) == 0.0]
 
-    M = nx.to_numpy_matrix(G, nodelist=nodelist, weight=weight)
-    N = len(G)
-    if N == 0:
-        return M
+    # power iteration: make up to 300 iterations
+    for _ in range(300):
+        xlast = x
+        x = dict.fromkeys(xlast.keys(), 0)
+        danglesum = alpha * sum(xlast[n] for n in dangling_nodes)
+        for n in x:
+            for nbr in W[n]:
+                x[nbr] += alpha * xlast[n] * W[n][nbr][weight]
+            x[n] += danglesum * dangling_weights[n] + (1.0 - alpha) * dangling_weights[n]
 
-    # Personalization vector
-    if personalization is None:
-        p = np.repeat(1.0 / N, N)
-    else:
-        missing = set(nodelist) - set(personalization)
-        if missing:
-            raise NetworkXError('Personalization vector dictionary '
-                                'must have a value for every node. '
-                                'Missing nodes %s' % missing)
-        p = np.array([personalization[n] for n in nodelist], dtype=float)
-        p /= p.sum()
+        err = sum([abs(x[n] - xlast[n]) for n in x])
+        if err < N*tol:
+            return x
 
-    # Dangling nodes
-    if dangling is None:
-        dangling_weights = p
-    else:
-        missing = set(nodelist) - set(dangling)
-        if missing:
-            raise NetworkXError('Dangling node dictionary '
-                                'must have a value for every node. '
-                                'Missing nodes %s' % missing)
-        # Convert the dangling dictionary into an array in nodelist order
-        dangling_weights = np.array([dangling[n] for n in nodelist],
-                                    dtype=float)
-        dangling_weights /= dangling_weights.sum()
-    dangling_nodes = np.where(M.sum(axis=1) == 0)[0]
-
-    # Assign dangling_weights to any dangling nodes (nodes with no out links)
-    for node in dangling_nodes:
-        M[node] = dangling_weights
-
-    M /= M.sum(axis=1)  # Normalize rows to sum to 1
-
-    return alpha * M + (1 - alpha) * p
-
-
-def pagerank_numpy(G, alpha=0.85, personalization=None, weight='weight',
-                   dangling=None):
-
-    if len(G) == 0:
-        return {}
-    M = google_matrix(G, alpha, personalization=personalization,
-                      weight=weight, dangling=dangling)
-    # use numpy LAPACK solver
-    eigenvalues, eigenvectors = np.linalg.eig(M.T)
-    ind = np.argmax(eigenvalues)
-    # eigenvector of largest eigenvalue is at ind, normalized
-    largest = np.array(eigenvectors[:, ind]).flatten().real
-    norm = float(largest.sum())
-    return dict(zip(G, map(float, largest / norm)))
 
 
 
@@ -96,9 +60,9 @@ def get_file_frame(str, val):
     count = 0
     success = True
     while success:
-        success,image = vidcap.read()
         if count == int(frame_num):
             cv2.imwrite(save_name, image)     # save frame as JPEG file
+        success,image = vidcap.read()
         count += 1
 
 
@@ -115,15 +79,20 @@ def load_images(output):
 
 if __name__ == '__main__':
 
-        filename = "C:\Users\sjjai\Desktop\Phase3\demovideos_10_sample_2.gspc"  #raw_input("Enter input path of the graph file: ")
-        vid_folder = "C:\Users\sjjai\Desktop\Phase3\DataR"  #raw_input("Enter path where the videos are stored: ")
-        m = 5   #raw_input("Enter the number of most significant frames to be found: ")
+        filename = raw_input("Enter input path of the graph file: ")
+        m = raw_input("Enter the number of most significant frames to be found: ")
+        vid_folder = raw_input("Enter path where the videos are stored: ")
 
-        h = Helper()
+        m = int(m)
+
         print "Processing... "
-        graph = h.parse_data_from_file(filename)
-        pr = pagerank_numpy(graph, alpha=0.85)
+        h = Helper()
 
+        graph = h.parse_data_from_file(filename)
+
+        pr = calc_pagerank(graph)
+
+        #pr = nx.pagerank(graph)
         sorted_pr = sorted(pr.iteritems(), key=lambda x:-x[1])[:m]
         print "Pagerank in sorted order:  "
         for x in sorted_pr:
