75b46f9c4b30f2d288ff0d09d9fed9d50e305da5
srikiranpanchavati <srikiran.phoenix@gmail.com>
2016-12-01 18:28:12 -0700

    updated personalised ascos

--- Phase3/src/task4/personalised_ascos.py
+++ Phase3/src/task4/personalised_ascos.py
@@ -3,8 +3,8 @@ from Phase3.src.task3.ascos import Ascos
 
 def personalized_ascos(videos_path, path, frame_input, m):
     ascos = Ascos()
-    graph, vertices = ascos.generate_adjacency_matrix(path, True, frame_input)
-    page_rank = ascos.ascos_similarity(graph)
+    graph, vertices, frame_indices = ascos.generate_adjacency_matrix(path, True, frame_input)
+    page_rank = ascos.ascos_similarity(graph, frame_indices)
 
     if m > len(page_rank):
         print "invalid m value"
